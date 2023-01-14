5cafe11fb1f27c37998200277a39db186bde78e3
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-01-14 15:58:07 +0530

    front page first draft

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+import Content from "./Content"
+
+export default function Main(){
+    return (
+        <>
+            <div>
+            <Navbar/>
+            <Content/>
+            </div>
+        </>
+    )
+}
---
+++ src/components/data/products.js
@@ -0,0 +1,114 @@
+const info=[
+    {
+        id: 1,
+        img:"./images/boat.jfif",
+        title: "Boat Wirless Headphone",
+        price: "1799"
+    },
+    {
+        id: 2,
+        img: "./images/dime.jfif",
+        title: "Dime Airdrops",
+        price: "899"
+    },
+    {
+        id: 3,
+        img: "./images/galaxyA23.jfif",
+        title: "Samsung Galaxy A23",
+        price: "16,890" 
+    },
+    {
+        id: 4,
+        img: "./images/galaxyF13.jfif",
+        title: "Samsung Galxy F13",
+        price: "14,000"
+    },
+    {
+        id: 5,
+        img: "./images/galaxyF23.jfif",
+        title: "Samsung Galaxy F23",
+        price: "15,690"
+    },
+    {
+        id: 6,
+        img: "./images/galaxyM13.jfif",
+        title: "Samsung Galxy M13",
+        price: "20,000" 
+    },
+    {
+        id: 7,
+        img: "./images/galaxyS21.jfif",
+        title: "Samsung Galaxy S21",
+        price: "21,560"
+    },
+    {
+        id: 8,
+        img: "./images/keyboard.png",
+        title: "Wirless Keyboard",
+        price: "1,299"
+    },
+    {
+        id: 9,
+        img: "./images/keyboard2.jfif",
+        title: "Gaming Keyboard",
+        price: "2,100"
+    },
+    {
+        id: 10,
+        img: "./images/mouse.jfif",
+        title: "Wireless Mouse",
+        price: "599"
+    },
+    {
+        id: 11,
+        img: "./images/NokiaG21.jfif",
+        title: "Nokia G21",
+        price: "17,899"
+    },
+    {
+        id: 12,
+        img: "./images/onePlus.jfif",
+        title: "One Plus headphones",
+        price: "2,199"
+    },
+    {
+        id: 13,
+        img: "./images/oppoA15.jfif",
+        title: "OPPO A15",
+        price: "20,100"
+    },
+    {
+        id: 14,
+        img: "./images/oppoA31.jfif",
+        title: "OPPO A31",
+        price: "19,999"
+    },
+    {
+        id: 15,
+        img: "./images/redmiNote9.jfif",
+        title: "Redmi Note 9",
+        price: "21,000"
+    },
+    {
+        id: 16,
+        img: "./images/redmi9Pro.jfif",
+        title: "Redmi 9 Pro",
+        price: "16,850"
+    },
+    {
+        id: 17,
+        img: "./images/redmiNote11.jfif",
+        title: "Redmi Note 11",
+        price: "17,650"
+    },
+    {
+        id: 18,
+        img: "./images/redmiPrime9.jfif",
+        title: "Redmi Prime 9",
+        price: "18,769"
+    }
+
+
+]
+
+export {info}
